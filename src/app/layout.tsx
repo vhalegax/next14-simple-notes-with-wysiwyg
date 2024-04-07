e7278e3e5c8da9a747cf8b1fc96d102b57d2a18c
wisnu setyawan <stephanus.ws@gmail.com>
2024-04-08 02:13:11 +0700

    feat: setup PWA

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -13,6 +13,17 @@ const fontSans = FontSans({
 export const metadata: Metadata = {
   title: "Simple Notes With WYSIWYG",
   description: "Generated simple notes with WYSIWYG",
+  generator: "Next.js",
+  manifest: "/manifest.json",
+  keywords: ["nextjs", "nextjs13", "next13", "pwa", "next-pwa"],
+  themeColor: [{ media: "(prefers-color-scheme: dark)", color: "#fff" }],
+  authors: [],
+  viewport:
+    "minimum-scale=1, initial-scale=1, width=device-width, shrink-to-fit=no, viewport-fit=cover",
+  icons: [
+    { rel: "apple-touch-icon", url: "icons/icon-128x128.png" },
+    { rel: "icon", url: "icons/icon-128x128.png" },
+  ],
 }
 
 export default function RootLayout({
